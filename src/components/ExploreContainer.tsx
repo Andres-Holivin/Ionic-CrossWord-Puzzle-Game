0082a92681597c8f47791bd9560effe98437c46f
andres holivin <andres12holivin@gmail.com>
2023-04-27 10:00:55 +0000

    add ionic react tailwind

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,20 @@
+import './ExploreContainer.css';
+
+interface ContainerProps {
+  name: string;
+}
+
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  return (
+    <div className="container">
+      <strong>{name}</strong>
+      <div className='m-4'>hello</div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
