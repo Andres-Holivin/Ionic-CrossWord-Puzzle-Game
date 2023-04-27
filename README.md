d362f51b6af23cdcabbf0e3ce60a02cda5edc8d7
andres holivin <andres12holivin@gmail.com>
2023-04-27 11:01:34 +0000

    fix tailwind

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ionic-CrossWord-Puzzle-Game
+sudo gem install cocoapods
+https://console.upstash.com/redis/54e7d50e-3fa5-4ef2-8af4-68fe05252b4e
--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+/* Tailwind styles */
 import './theme/tailwind.css';
 
 setupIonicReact();
