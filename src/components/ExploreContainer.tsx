d8d9773a0d8e145a38a91afaac08546d3d316cb6
andres holivin <andres12holivin@gmail.com>
2023-04-28 09:25:50 +0000

    add board

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,18 +1,38 @@
+import { IonCol, IonGrid, IonInput, IonRow } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
   name: string;
 }
-
+let count = 15
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <div className='m-4'>hello</div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div className="">
+      <IonGrid >
+        {
+          [...Array(count)].map((v, j) =>
+            <IonRow>
+              {
+                [...Array(count)].map((v, i) =>
+                  <IonCol className='p-0 border-solid border-2 w-12 h-12' >
+                    <div className='w-full h-full'>
+                      <div style={{ "fontSize": "0.6em" }} className=' absolute z-10' >
+                        {i + (j * 10) + 1}
+                      </div>
+                      <div className='h-full'>
+                        <input maxLength={1}
+                        className='uppercase text-lg text-center bg-transparent
+                        outline-none w-full h-full'></input>
+                      </div>
+                    </div>
+                  </IonCol>
+                )
+              }
+            </IonRow>
+          )
+        }
+
+      </IonGrid>
     </div>
   );
 };
